ceac34f877c906533dc7763538c29a7791bb3684
Teo <teo.bostiog@yahoo.com>
2025-01-16 15:28:13 +0200

    sorting table

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 import { BasicTable } from "./components/BasicTable";
+import { SortingTable } from "./components/SortingTable";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <BasicTable />
+      <SortingTable />
     </div>
   );
 }
