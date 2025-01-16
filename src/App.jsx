7d32450ce810f625f029cb8a841c34c6bffbebbc
Teo <teo.bostiog@yahoo.com>
2025-01-16 15:39:56 +0200

    global filtering

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import { BasicTable } from "./components/BasicTable";
 import { SortingTable } from "./components/SortingTable";
+import { FilteringTable } from "./components/FilteringTable";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <SortingTable />
+      <FilteringTable />
     </div>
   );
 }
